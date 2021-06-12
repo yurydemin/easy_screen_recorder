252d0dc1d9bfc200ca76200f8ab61bb88a2a4e20
yuriy_demin <y.y.demin@gmail.com>
2021-06-13 02:34:46 +0500

    close foreground service experiments

--- lib/src/views/home_view.dart
+++ lib/src/views/home_view.dart
@@ -1,7 +1,4 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_foreground_plugin/flutter_foreground_plugin.dart';
 import 'package:flutter_screen_recording/flutter_screen_recording.dart';
 import 'package:open_file/open_file.dart';
 import 'package:permission_handler/permission_handler.dart';
@@ -30,41 +27,6 @@ class _HomeViewState extends State<HomeView> {
     requestPermissions();
   }
 
-  // @override
-  // Widget build(BuildContext context) {
-  //   return MaterialApp(
-  //     home: Scaffold(
-  //       appBar: AppBar(
-  //         title: const Text('Foreground Service Example'),
-  //       ),
-  //       body: Center(
-  //         child: Column(
-  //           children: <Widget>[
-  //             ElevatedButton(
-  //               child: Text("START"),
-  //               onPressed: () {
-  //                 startForegroundService();
-  //               },
-  //             ),
-  //             ElevatedButton(
-  //               child: Text("STOP"),
-  //               onPressed: () async {
-  //                 await FlutterForegroundPlugin.stopForegroundService();
-  //               },
-  //             ),
-  //             ElevatedButton(
-  //               child: Text("Force Crash"),
-  //               onPressed: () {
-  //                 exit(1);
-  //               },
-  //             )
-  //           ],
-  //         ),
-  //       ),
-  //     ),
-  //   );
-  // }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -90,27 +52,6 @@ class _HomeViewState extends State<HomeView> {
         ],
       ),
     );
-  }
-
-  void startForegroundService() async {
-    await FlutterForegroundPlugin.setServiceMethodInterval(seconds: 5);
-    //await FlutterForegroundPlugin.setServiceMethod(globalForegroundService);
-    await FlutterForegroundPlugin.startForegroundService(
-      holdWakeLock: false,
-      onStarted: () {
-        print("Foreground on Started");
-      },
-      onStopped: () {
-        print("Foreground on Stopped");
-      },
-      title: "Flutter Foreground Service",
-      content: "This is Content",
-      iconName: "ic_stat_hot_tub",
-    );
-  }
-
-  void globalForegroundService() {
-    print("current datetime is ${DateTime.now()}");
   }
 
   startScreenRecord(bool audio) async {
